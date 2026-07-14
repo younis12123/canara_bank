d96cc3b203fbdd457313e91f3ae904866359213b
Ali <shaik.younis1212@gmail.com>
2026-07-14 15:54:29 +0530

    first save

---
+++ src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+
+
+
+
+}
